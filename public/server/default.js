f3b500d5911a5a347bdb037528dd8bf1c44c4229
Wildhoney <adam.timberlake@gmail.com>
2015-06-10 00:05:37 +0100

    Added initial setup and deployment

---
+++ public/server/default.js
@@ -0,0 +1,20 @@
+(function($process) {
+
+    "use strict";
+
+    var express = require('express'),
+        app     = express(),
+        colour  = require('cli-color'),
+        pad     = require('pad'),
+        server  = require('http').createServer(app),
+        port    = ($process.env.PORT || 5000);
+
+    app.use(express.static(__dirname + '/..'));
+    server.listen(port);
+
+    var header = colour.xterm(231).bgXterm(161),
+        yellow = colour.xterm(221).bgXterm(232);
+
+    console.log(header(' Travelet:', yellow(' Running on port ' + port)));
+
+})(process);
